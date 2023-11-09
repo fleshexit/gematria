d00f8027384d6fbd19806d0a62251cf0b573eef2
chrlie-w <chrlie.w@proton.me>
2023-11-09 16:24:20 +0000

    Update

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from ciphers import Ciphers
 
 
-print(Ciphers().alphanumeric_qabbala())
-print()
-print(Ciphers().english_ordinal())
+def calculate_alphanumeric_qabbala_value(user_input):
+    user_input = user_input.upper()  
+    ciphers = Ciphers()
+    ciphers.alphanumeric_qabbala() 
+
+    value = 0
+    for char in user_input:
+        if char in ciphers.gematria:
+            value += ciphers.gematria[char]
+
+    return value
+
+user_input = input("Enter a string: ")
+result = calculate_alphanumeric_qabbala_value(user_input)
+print(f"The alphanumeric qabbala value of '{user_input}' is: {result}")
+
