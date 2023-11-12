e4d38fdd49f9875f2f8a1b255bdf86ced1d203c1
chrlie-w <chrlie.w@proton.me>
2023-11-12 22:38:21 +0000

    Ciphers class

--- ciphers.py
+++ ciphers.py
@@ -1,18 +1,24 @@
 
 class Ciphers:
 
-    def __init__(self):
+    def __init__(self, cipher='Alphanumeric Qabbala'):
         self.gematria = {}
+
+        if cipher == 'Alphanumeric Qabbala':
+            self.alphanumeric_qabbala()
+        elif cipher == 'English Ordinal':
+            self.english_ordinal()
 
     def alphanumeric_qabbala(self):
         for i in range(36):
             if i < 10:
-                self.gematria[i] = i
+                self.gematria[i] = f'{i}'
             else:
                 self.gematria[chr(65 + i - 10)] = i
         return self.gematria
-    
+
     def english_ordinal(self):
         for i in range(26):
             self.gematria[chr(65 + i)] = i + 1
         return self.gematria
+
