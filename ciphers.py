c0074ca2e254d665b1cb925949cad933a9ced109
chrlie-w <chrlie.w@proton.me>
2023-11-13 16:39:32 +0000

    Class replacement

--- ciphers.py
+++ ciphers.py
@@ -1,15 +1,10 @@
+class AlphanumericQabbala:
 
-class Ciphers:
-
-    def __init__(self, cipher='Alphanumeric Qabbala'):
+    def __init__(self):
         self.gematria = {}
+        self.create_gematria()
 
-        if cipher == 'Alphanumeric Qabbala':
-            self.alphanumeric_qabbala()
-        elif cipher == 'English Ordinal':
-            self.english_ordinal()
-
-    def alphanumeric_qabbala(self):
+    def create_gematria(self):
         for i in range(36):
             if i < 10:
                 self.gematria[str(i)] = i
@@ -17,8 +12,31 @@ class Ciphers:
                 self.gematria[chr(65 + i - 10)] = i 
         return self.gematria
 
-    def english_ordinal(self):
+    def calculate(self, user_input):
+        user_input = user_input.upper()
+        value = 0
+        for char in user_input: # iterate through string
+            if char in self.gematria: # check if char is in dict
+                value += self.gematria[char] # add value to total
+
+        return value
+
+class EnglishOrdinal:
+
+    def __init__(self):
+        self.gematria = {}
+        self.create_gematria()
+
+    def create_gematria(self):
         for i in range(26):
             self.gematria[chr(65 + i)] = i + 1
         return self.gematria
 
+    def calculate(self, user_input):
+        user_input = user_input.upper()
+        value = 0
+        for char in user_input:
+            if char in self.gematria:
+                value += self.gematria[char]
+                
+        return value
